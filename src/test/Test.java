0e86195611816886549651712e3634ae6c5b450a
KP CHILOANE <201762927@ump.ac.za>
2021-05-14 14:58:39 +0200

    first commit

---
+++ src/test/Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author 201762927
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      int x = 3;
+      int y = 4;
+      int sum =x +y;
+       System.out.println("the sum of the two number is equal to : "+sum);
+             
+    }
+    
+}
